b2fb60717d3d232d9218be9ce6dd124b8acd70fe
Hareesh-Raj <125631814+Hareesh-Raj@users.noreply.github.com>
2023-03-30 20:46:39 +0530

    Added Review Comments

--- SiriusLocations/js/script.js
+++ SiriusLocations/js/script.js
@@ -1,37 +1,32 @@
-import locationData from "./locations.json" assert { type: 'json'};
+import locationData from "./locations.json" assert { type: "json" };
 $(function () {
-    $("#tabs").tabs();
+  $("#tabs").tabs();
+  $("#accordion").accordion({
+    active: true,
   });
-  
-  $(function () {
-    $("#accordion").accordion({
-      active: true,
-    });
-  });
-  function addLocationData()
-  {
-    const table = $("<table>");
-    table.addClass("location-table");
-    for(let data of locationData)
-    {
-        const row = $("<tr>");
-        const imageData = $("<td>");
-        const stateData = $("<td>");
-        const cityData = $("<td>");
-        const contactData = $("<td>");
+});
 
-        const image = $("<img>");
-        image.attr("src",`../images/flags/${data.country}.png`);
-        imageData.append(image);
-        row.append(imageData);
-        stateData.text(data.country);
-        row.append(stateData);
-        cityData.text(data.city);
-        row.append(cityData);
-        contactData.text(data.contact);
-        row.append(contactData);
-        table.append(row);
-    }
-    $("#tabs-3").append(table);
+function addLocationData() {
+  var table = $("<table>");
+  table.addClass("location-table");
+  $.each(locationData, function (data, val) {
+    var row = $("<tr>");
+    $.each(val, function (data1, val1) {
+      if (data1 == "country") {
+        row.append(
+          $("<td>").append(
+            $("<img>").attr("src", `../images/flags/${val1}.png`)
+          )
+        );
+      } else {
+        row.append($("<td>").text(val1));
+      }
+    });
+    table.append(row);
+  });
+  $.each(locationData, function (data, val) {
+    console.log(val["country"]);
+  });
+  $("#tabs-3").append(table);
 }
-  addLocationData();
+addLocationData();
