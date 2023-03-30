a83253825211917bf4ba58502573ad5c0d753cd5
Hareesh-Raj <125631814+Hareesh-Raj@users.noreply.github.com>
2023-03-30 20:47:54 +0530

    Added Review Comments

--- Jquery_Blender_Movies/js/script.js
+++ Jquery_Blender_Movies/js/script.js
@@ -1,121 +1,102 @@
-$.ajax({
-  url: "https://mocki.io/v1/4da47fc5-bbf3-4e41-b35f-c88a584bc4b0",
-  success: function (videoData) {
-    //adding video
-    const video = $("<video>");
-    video.attr("src", videoData.videoUrl);
-    video.attr("width", "100%");
-    video.attr("controls", true);
-    video.attr(
-      "poster",
-      "https://www.slashcam.de/images/news/sprite_fright1-16857_PIC1.jpg"
+$(document).ready(function () {
+  $.ajax({
+    url: "https://mocki.io/v1/4da47fc5-bbf3-4e41-b35f-c88a584bc4b0",
+    success: function (videoData) {
+      //adding video
+      videoContainer.append(
+        $("<video>").attr({
+          src: videoData.videoUrl,
+          width: "100%",
+          controls: true,
+          poster:
+            "https://www.slashcam.de/images/news/sprite_fright1-16857_PIC1.jpg",
+        })
+      );
+      //adding video title
+      var videoContent = $("<div>");
+
+      videoContent.append(
+        $("<span>").attr("id", "video-title").text(videoData.title)
+      );
+
+      //adding video description
+
+      videoContent.append(
+        $("<p>").text(videoData.description).attr("id", "video-description")
+      );
+
+      //adding comments section
+      const commentsSection = $("<div>").addClass("display-comment");
+      //Comment title
+      commentsSection.append(
+        $("<span>").attr("id", "comments").text("Comments")
+      );
+
+      $.each(videoData.comments, function (data, comment) {
+        //comments content
+        const commentsContent = $("<div>");
+        commentsContent.addClass("personal-comments");
+        //commenter image
+        const commenter_image = $("<div>").addClass("comment-image");
+        const image = $("<img>");
+        image.attr("src", comment["image"]);
+        image.attr("alt", "commenter image");
+        commenter_image.append(image);
+        //comment data display
+        var comment_data = $("<div>").addClass("comment-data-display");
+        //commenter name
+        comment_data.append(
+          $("<span>").attr("id", "commenter-name").text(comment["name"])
+        );
+        //comment data
+        comment_data.append(
+          $("<p>").attr("id", "comments-content").text(comment["comment"])
+        );
+
+        commentsContent.append(commenter_image);
+        commentsContent.append(comment_data);
+        commentsSection.append(commentsContent);
+      });
+
+      videoContainer.append(videoContent);
+      // adding ruler
+      videoContainer.append($("<div>").addClass("ruler"));
+      videoContainer.append(commentsSection);
+      //responsive ruler
+      videoContainer.append(
+        $("<div>").addClass("responsive-ruler").addClass("ruler")
+      );
+    },
+  });
+
+  function addHeaderDetails() {
+    $("#username").text("Steffy Jerry");
+    $("#userProfile").append(
+      $("<img>").attr({
+        src: "./images/user-image.png",
+        alt: "user-profile-image",
+      })
     );
-    videoContainer.append(video);
+  }
 
-    //adding video title
-    const videoContent = $("<div>");
-    const title = $("<span>");
-    title.attr("id", "video-title");
-    title.html(videoData.title);
-    videoContent.append(title);
-
-    //adding video description
-
-    const description = $("<p>");
-    description.text(videoData.description);
-    description.attr("id", "video-description");
-    videoContent.append(description);
-
-    // adding ruler
-    const ruler = $("<div>");
-    ruler.addClass("ruler");
-    //adding comments section
-    const commentsSection = $("<div>");
-    commentsSection.addClass("display-comment");
-    //Comment title
-    const commentTitle = $("<span>");
-    commentTitle.attr("id", "comments");
-    commentTitle.text("Comments");
-    commentsSection.append(commentTitle);
-
-    for (let comment of videoData.comments) {
-      //comments content
-      const commentsContent = $("<div>");
-      commentsContent.addClass("personal-comments");
-      //commenter image
-      const commenter_image = $("<div>");
-      commenter_image.addClass("comment-image");
-      const image = $("<img>");
-      image.attr("src", comment.image);
-      image.attr("alt", "commenter image");
-      commenter_image.append(image);
-      //comment data display
-      const comment_data = $("<div>");
-      comment_data.addClass("comment-data-display");
-      //commenter name
-      const commenter_name = $("<span>");
-      commenter_name.attr("id", "commenter-name");
-      commenter_name.text(comment.name);
-      //comment data
-      const comment_content = $("<p>");
-      comment_content.attr("id", "comments-content");
-      comment_content.text(comment.comment);
-
-      comment_data.append(commenter_name);
-      comment_data.append(comment_content);
-
-      commentsContent.append(commenter_image);
-      commentsContent.append(comment_data);
-
-      commentsSection.append(commentsContent);
-    }
-    const responsive_ruler = $("<div>");
-    responsive_ruler.addClass("responsive-ruler");
-    responsive_ruler.addClass("ruler");
-
-    videoContainer.append(videoContent);
-    videoContainer.append(ruler);
-    videoContainer.append(commentsSection);
-    videoContainer.append(responsive_ruler);
-  },
-  error: function (XMLHttpRequest, textStatus, errorThrown) {
-    alert("Status: " + textStatus);
-    alert("Error: " + errorThrown);
-  },
+  const videoContainer = $("#video-content");
+  $.ajax({
+    url: "https://mocki.io/v1/8c9b378b-d248-4203-93b0-b8e7659ac346",
+    success: function (posterData) {
+      // const upcomingProjectsSection = $("#upcoming-projects");
+      $.each(posterData, function (data, poster) {
+        $("#upcoming-projects").append(
+          $("<div>")
+            .addClass("project-image")
+            .append(
+              $("<img>").attr({ src: poster["imageUrl"], alt: poster["title"] })
+            )
+        );
+      });
+    },
+  });
+  function main() {
+    addHeaderDetails();
+  }
+  main();
 });
-
-function addHeaderDetails() {
-  console.log("helloo");
-  const username = $("#username");
-  username.html("Steffy Jerry");
-  const userimage = $("#userProfile");
-  const profileImage = $("<img>");
-  profileImage.attr("src", "./images/user-image.png");
-  profileImage.attr("alt", "user-profile-image");
-  userimage.append(profileImage);
-}
-
-const videoContainer = $("#video-content");
-$.ajax({
-  url: "https://mocki.io/v1/8c9b378b-d248-4203-93b0-b8e7659ac346",
-  success: function (posterData) {
-    const upcomingProjectsSection = $("#upcoming-projects");
-    for (let poster of posterData) {
-      const project_image = $("<div>");
-      project_image.addClass("project-image");
-      const upcoming_image = $("<img>");
-      upcoming_image.attr("src", poster.imageUrl);
-      upcoming_image.attr("alt", poster.title);
-      project_image.append(upcoming_image);
-      upcomingProjectsSection.append(project_image);
-    }
-  },
-  error: function (XMLHttpRequest, textStatus, errorThrown) {
-    alert("Status: " + textStatus);
-    alert("Error: " + errorThrown);
-  },
-});
-function main() {
-  addHeaderDetails();
-}
-main();
